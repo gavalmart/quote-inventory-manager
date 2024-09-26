3ba3802b9d745a7540cc6f477121f6714063a8d3
Gustavo Valdes <valdes.gustavo@gmail.com>
2024-09-26 17:48:58 -0600

    Changes to urls.py to run the app on Django 4.2.16

--- quote-inventory-manager/gentelella/urls.py
+++ quote-inventory-manager/gentelella/urls.py
@@ -13,14 +13,34 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+
+# to run using Django 2.1
+# from django.conf.urls import url, include
+# from django.contrib import admin
+
+# to run using Django 4.2
+from django.urls import include, path, re_path
 from django.contrib import admin
 
+
+# to run using Django 2.1
+# urlpatterns = [
+#     url(r'^admin/', admin.site.urls),
+
+#     # app/ -> Genetelella UI and resources
+#     url(r'^app/', include('app.urls')),
+#     url(r'^', include('app.urls')),
+
+# ]
+
+# to run using Django 4.2
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    re_path(r'^admin/', admin.site.urls),
 
     # app/ -> Genetelella UI and resources
-    url(r'^app/', include('app.urls')),
-    url(r'^', include('app.urls')),
+    path(r'^app/', include('app.urls')),
+    path(r'^', include('app.urls')),
+    path('', include('app.urls')),
 
 ]
